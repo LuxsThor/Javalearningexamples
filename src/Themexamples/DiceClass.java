88a0b1b13bbbd6f66bfcce9ad04286831d35688a
LuxsThor <lucas.thorbecke@bluewin.ch>
2018-10-17 16:06:07 +0200

    wrote a dice class

---
+++ src/Themexamples/DiceClass.java
@@ -0,0 +1,36 @@
+package Themexamples;
+import java.util.* ;
+
+public class DiceClass {
+    //instance variables of a dice object
+    // private variables are only accessible to methods inside the class
+    private int numDice;
+    private Random random;
+    // constructor for no parameters. The name is always the same as the classname
+    public DiceClass(){
+        this.numDice = 1;
+        this.random = new Random();
+    }
+    // constructor for some parameters
+    public DiceClass(int numberofdice){
+        this.numDice = numberofdice;
+        this.random = new Random();
+    }
+
+    public int rollDice(){
+        int sum = 0;
+        for (int i = 0; i < numDice; i++){
+            sum += random.nextInt(6) + 1;
+        }
+        return sum;
+    }
+
+    public void setDice(int setnumberofdice){
+        this.numDice = setnumberofdice;
+    }
+
+    public int returnDice(){
+        return this.numDice;
+    }
+
+}
